4bea511cf342a9401efc1dcb831def7e8add8ea3
theincredibleplayer <54246318+theincredibleplayer@users.noreply.github.com>
2025-04-28 18:50:05 +0300

    update

--- UART_CONFIG.h
+++ UART_CONFIG.h
@@ -3,13 +3,9 @@
 
 void UART0_Init(void);
 void UART0_SendChar(char c);
-void UART0_SendString(const char *str);
-void UART0_SendNumber(int num);
 
 void UART5_Init(void);
 char UART5_ReceiveChar(void);
-char *UART5_ReceiveString(char *string);
-
 
 void PORTB_Init(void);
 
