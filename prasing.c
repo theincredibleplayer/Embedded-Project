9af959a7fc531bd5c1c4b5fd0d718863fe65ebb3
Abdelrahman-Emad688 <AbdelrahmanEmad-eldinn.UNI@hotmail.com>
2025-04-30 02:56:00 +0300

    Distance Calculation
    this is adds a distance calculation function to prasing.c

--- prasing.c
+++ prasing.c
@@ -1,13 +1,22 @@
+#include "prasing.h"
 #include <stdlib.h>
 #include <string.h>
 #include "UART_CONFIG.h"
 #include "stdio.h"
+#include "math.h" // to use mathematical functions to calculate distance
+
+
 
 char GPS_ReqName[]="$GPRMC,";
 char GPS[80];
 char GPS_Array[20][20];
 char *token;
 float My_Longitude, My_Latitude;
+float Loc_Longitude[5], Loc_Latitude[5]; //the lat. & long. of pre-saved locations
+char Location_Names[6][20]; // 5 locations indexed from 0 to 4 and a sixth array called "you are far" when no place is in range
+char Location_index = 5; //index to choose location ,set initially to 5 which is "you are far"
+float R = 6378000; // radius of the globe
+float Distance_Arr[5];
 
 void GPS_ReadData(){
 	char Inputchar;
@@ -61,9 +70,34 @@ char No_tokens=0;
 	
 	}
 
-
-
 }
+
+// calculates the distance and checks which location is near
+void Distance(){
+	char i = 0;
+	float My_X = R * My_Longitude;
+	float My_Y = R * My_Latitude;
+	while(i < 5){
+	float Loc_X = R * Loc_Longitude[i];	
+	float Loc_Y = R * Loc_Latitude[i];	
+	float vec_X = My_X - Loc_X;
+	float vec_Y = My_Y - Loc_Y;
+	float distance = sqrt(pow(vec_X,2) + pow(vec_Y,2));
+	Distance_Arr[i] = distance;
+	if (distance <= 10) Location_index = i;
+	}
+}
+
+
+// sends the location to the LCD
+/*
+void Location_Identification(char index){
+ char My_Location = strcpy(My_Location,Location_Names[Location_index]);
+ UART0_SendString(My_Location);
+
+} */
+
+
 
 
 
