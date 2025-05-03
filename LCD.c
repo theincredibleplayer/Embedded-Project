6ddc1919836f272ad813ea5fbf320c21f12cdc8a
ibrahimm101 <ibrahimwaw27@gmail.com>
2025-05-03 22:18:39 +0300

    Update LCD.c

--- LCD.c
+++ LCD.c
@@ -1,24 +1,18 @@
 #include "C:\Keil\Labware\inc\tm4c123gh6pm.h"
-
-// RS = PD0
-// RW = PD1
-// EN = PD2
-
-// D0 = PB7
-// D1 = PB6
-// D2 = PB5
-// D3 = PB4
-// D4 = PB3
-// D5 = PB2
-// D6 = PB1
-// D7 = PB0
+#include <stdint.h>
+// RS = PD0 ; RW = PD1 ; EN = PD2 ;  D0 = PB7 ; D1 = PB6 ; D2 = PB5 ; D3 = PB4 ; D4 = PB3;  D5 = PB2;  D6 = PB1;  D7 = PB0
 
 void delay_ms(long milliseconds);
-void sendToDataPort(unsigned char data);
-void writeLCDData(unsigned char data);
-void sendLCDCommand(unsigned char command);
-void writeLCDString(char *str, unsigned char length);
-void initializeLCD(void);
+
+void send_To_Data_Port(unsigned char data); //Maps each bit of the data byte to the correct Port B pin 
+
+void write_LCD_Data(unsigned char data);   //Sends data (a character) to be displayed on the LCD.
+
+void send_LCD_Command(unsigned char command);  //Sends a command to the LCD (clear display, set cursor position).
+
+void write_LCD_String(char *str, unsigned char length); //Writes a string of given length to the LCD.
+
+void initialize_LCD(void); //Sends standard initialization commands to configure the LCD display.
 
 int main() 
 {
@@ -31,16 +25,17 @@ int main()
     GPIO_PORTD_DIR_R |= 0x07;  // Set PD0-PD2 as output
 
     delay_ms(10000);
-    initializeLCD();
-    sendLCDCommand(0x80);  // Move cursor to first row, first column
-    writeLCDString("Ibrahim", 7);
+    initialize_LCD();
+    send_LCD_Command(0x80);  // Move cursor to first row, first column
+    write_LCD_String("Ibrahim", 7);
 
-    while(1) {}
+    while(1) {
+		}
 }
 
-void writeLCDData(unsigned char data)
+void write_LCD_Data(unsigned char data)
 {
-    sendToDataPort(data);
+    send_To_Data_Port(data);
     GPIO_PORTD_DATA_R &= ~(1<<1);  // RW = 0 (write)
     GPIO_PORTD_DATA_R |=  (1<<0);  // RS = 1 (data)
     GPIO_PORTD_DATA_R |=  (1<<2);  // EN = 1
@@ -48,9 +43,9 @@ void writeLCDData(unsigned char data)
     GPIO_PORTD_DATA_R &= ~(1<<2);  // EN = 0
 }
 
-void sendLCDCommand(unsigned char command)
+void send_LCD_Command(unsigned char command)
 {
-    sendToDataPort(command);
+    send_To_Data_Port(command);
     GPIO_PORTD_DATA_R &= ~(1<<1);  // RW = 0
     GPIO_PORTD_DATA_R &= ~(1<<0);  // RS = 0 (command)
     GPIO_PORTD_DATA_R |=  (1<<2);  // EN = 1
@@ -58,40 +53,59 @@ void sendLCDCommand(unsigned char command)
     GPIO_PORTD_DATA_R &= ~(1<<2);  // EN = 0
 }
 
-void writeLCDString(char *str, unsigned char length)
+void write_LCD_String(char *str, unsigned char length)
 {
 	char i;
     for( i = 0; i < length; i++) {
-        writeLCDData(str[i]);
+        write_LCD_Data(str[i]);
     }
 }
 
-void initializeLCD(void)
+void initialize_LCD(void)
 {
-    sendLCDCommand(0x38);  // 8-bit, 2 line, 5x8 font
-    sendLCDCommand(0x06);  // Increment cursor
-    sendLCDCommand(0x0C);  // Display on, cursor off
-    sendLCDCommand(0x01);  // Clear display
+    send_LCD_Command(0x38);  
+	
+    send_LCD_Command(0x06);  
+	
+    send_LCD_Command(0x0C);  
+	
+    send_LCD_Command(0x01);  
 }
 
-void sendToDataPort(unsigned char data)
+void send_To_Data_Port(unsigned char data)
 {
-    // Mapping data bits to PB7-PB0
-    if(data & 0x01) GPIO_PORTB_DATA_R |= (1<<7);
+    
+    if(data & 0x01) 
+			GPIO_PORTB_DATA_R |= (1<<7);
+	
    	else GPIO_PORTB_DATA_R &= ~(1<<7);
-    if(data & 0x02) GPIO_PORTB_DATA_R |= (1<<6);
+	
+    if(data & 0x02)
+			GPIO_PORTB_DATA_R |= (1<<6);
   	else GPIO_PORTB_DATA_R &= ~(1<<6);
-    if(data & 0x04) GPIO_PORTB_DATA_R |= (1<<5);
+		
+    if(data & 0x04)
+			GPIO_PORTB_DATA_R |= (1<<5);
   	else GPIO_PORTB_DATA_R &= ~(1<<5);
-    if(data & 0x08) GPIO_PORTB_DATA_R |= (1<<4);
+		
+    if(data & 0x08)
+			GPIO_PORTB_DATA_R |= (1<<4);
   	else GPIO_PORTB_DATA_R &= ~(1<<4);
-    if(data & 0x10) GPIO_PORTB_DATA_R |= (1<<3);
+		
+    if(data & 0x10)
+			GPIO_PORTB_DATA_R |= (1<<3);
   	else GPIO_PORTB_DATA_R &= ~(1<<3);
-    if(data & 0x20) GPIO_PORTB_DATA_R |= (1<<2); 
+		
+    if(data & 0x20)
+			GPIO_PORTB_DATA_R |= (1<<2); 
 	  else GPIO_PORTB_DATA_R &= ~(1<<2);
-    if(data & 0x40) GPIO_PORTB_DATA_R |= (1<<1); 
+		
+    if(data & 0x40)
+			GPIO_PORTB_DATA_R |= (1<<1); 
 	  else GPIO_PORTB_DATA_R &= ~(1<<1);
-    if(data & 0x80) GPIO_PORTB_DATA_R |= (1<<0); 
+		
+    if(data & 0x80) 
+			GPIO_PORTB_DATA_R |= (1<<0); 
 	  else GPIO_PORTB_DATA_R &= ~(1<<0);
 }
 
