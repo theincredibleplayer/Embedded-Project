e5ebd9407e6f921427a93432946812e10044c793
theincredibleplayer <54246318+theincredibleplayer@users.noreply.github.com>
2025-05-04 16:16:21 +0300

    update

--- prasing.d
+++
@@ -1,9 +0,0 @@
-.\prasing.o: prasing.c
-.\prasing.o: prasing.h
-.\prasing.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
-.\prasing.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\prasing.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
-.\prasing.o: UART_CONFIG.h
-.\prasing.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
-.\prasing.o: parsing.h
-.\prasing.o: C:\Keil\ARM\ARMCC\bin\..\include\math.h
